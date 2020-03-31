604d42fd790f9fcc179502758d9b0fe291bdf8a1
forgehe <liao098@gmail.com>
2020-03-30 21:52:31 -0700

    exercise 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -11,4 +11,17 @@ surrey = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: fals
 whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
 yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
 
-p Store.where(mens_apparel: true)
+@mens_stores = Store.where(mens_apparel: true)
+@mens_stores.each do |store|
+  puts store.name
+  puts store.annual_revenue
+end
+
+puts 
+
+@womens_stores = Store.where("womens_apparel = true AND annual_revenue < 1000000")
+
+@womens_stores.each do |store|
+  puts store.name
+  puts store.annual_revenue
+end
